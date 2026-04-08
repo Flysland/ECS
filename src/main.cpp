b62e13570a6a5545cb58893b80624d50b1683e80
Flysland <corentin.abriel@gmail.com>
2026-04-08 14:28:13 +0200

    Init project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+////////////////////////
+//
+//  Created: Wed Apr 08 2026
+//  File: main.cpp
+//
+////////////////////////
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from main" << std::endl;
+    return 0;
+}
